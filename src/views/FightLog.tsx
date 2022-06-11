296fdecedc71842b3b34fd1e4b7a13b9cfbebdd3
Dawid Kordos <dkordos82@wp.pl>
2022-06-12 01:46:22 +0200

    Added fight log view and updated styles for logs.

---
+++ src/views/FightLog.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+    log: string[];
+}
+
+export const FightLog = (props: Props) => {
+    return (
+        <>
+            <h1 className="article__title">Fight log</h1>
+            <ol className="article__log-list">
+                {[...props.log].map((entry, i) => <li key={i} className="article__log-item"> {entry}</li>)}
+            </ol>
+        </>
+    )
+}

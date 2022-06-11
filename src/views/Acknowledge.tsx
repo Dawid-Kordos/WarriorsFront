d5a31a8b2155e4b0d2714afc2d5844c0d6b9885f
Dawid Kordos <dkordos82@wp.pl>
2022-06-11 20:43:01 +0200

    Added acknowledge view for rendering info that the adding process was implemented correctly.

---
+++ src/views/Acknowledge.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import warrior from '../graphics/warrior2.jpg';
+
+interface Props {
+    name: string;
+}
+
+export const Acknowledge = (props: Props) => (
+    <>
+        <h1 className="article__title">Warrior <span className="article__paragraph">'{props.name}'</span> has been
+            registered!</h1>
+        <img className="article__img" src={warrior} alt="warrior"/>
+        <h3 className="article__sub-title">Now you can test yourself!
+            <Link to="/arena/fight-form" className="article__link"> Make a fight!</Link>
+        </h3>
+    </>
+)
